f5dd11e0b6226aa1f25bc451b4a4c6bbf8bc9661
hustender <belawoe2008@gmail.com>
2024-12-27 00:54:54 +0100

    color for other_os.go@v1.1.2

--- other_os.go
+++ other_os.go
@@ -4,11 +4,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/hustender/goBeyond/cmd"
 	"os"
 	"runtime"
 )
 
 func main() {
-	fmt.Printf("This program only runs on Windows. Your current OS is: %s\n", runtime.GOOS)
+	fmt.Println(cmd.Error(fmt.Sprintf("This program only runs on Windows. Your current OS is: %s\n", runtime.GOOS)))
 	os.Exit(1)
 }
